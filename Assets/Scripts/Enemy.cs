abb74fd89d8c026513a1266bdc5d02d142c51303
Oleksandr <145229696+KadeSan4uk@users.noreply.github.com>
2025-01-15 21:14:13 +0200

    Enemy Hits

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,10 +3,22 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject destroyVFX;
+    [SerializeField] int hitPoint = 3;
 
     private void OnParticleCollision(GameObject other)
-    {       
-        Instantiate(destroyVFX, transform.position, Quaternion.identity);
-        Destroy(this.gameObject);
+    {
+        ProcessHit();
+
+    }
+
+    private void ProcessHit()
+    {
+        hitPoint--;
+
+        if (hitPoint <= 0)
+        {
+            Instantiate(destroyVFX, transform.position, Quaternion.identity);
+            Destroy(this.gameObject);
+        }
     }
 }
