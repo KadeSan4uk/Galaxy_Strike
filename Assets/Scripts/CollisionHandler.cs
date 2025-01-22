c62eb759eebbeac5d7cf519f5618d41f88d52154
Oleksandr <145229696+KadeSan4uk@users.noreply.github.com>
2025-01-22 20:34:21 +0200

    Reload Scene using Coroutine

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -3,9 +3,11 @@ using UnityEngine;
 public class CollisionHandler : MonoBehaviour
 {
     [SerializeField] GameObject destroyVFX;
+    [SerializeField] GameSceneManager gameSceneManager;
 
     private void OnTriggerEnter(Collider other)
     {
+        gameSceneManager.ReloadLevel();
         Instantiate(destroyVFX, transform.position, Quaternion.identity);
         Destroy(gameObject);
     }
